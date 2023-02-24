ec1525b25885ce80282504524cf92eef9af2e0bb
Dev_Latte <78749820+HelpWizz@users.noreply.github.com>
2023-02-24 21:02:21 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 local loadstring, getgenv, setclipboard, tablefind, UserInputService = loadstring, getgenv, setclipboard, table.find, game:GetService("UserInputService")
 
 
-local library = loadstring(game:GetObjects("rbxassetid://12598305896")[1].Source)()
+local library = loadstring(game:GetObjects("rbxassetid://12598305896").Source)()
 local MainFrame = library:CreateWindow({
 	Name = "SympleHub",
 })
